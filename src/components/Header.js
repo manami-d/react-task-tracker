ddede5e1dc8cec0b03daed0bcfef9a7f11932dfc
manami-d <manami.davidson@hotmail.com>
2021-02-20 10:49:06 +1100

    Complete up to Events

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Click')
+  }
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red', 
+//   backgroundColor: 'black'
+// }
+export default Header
